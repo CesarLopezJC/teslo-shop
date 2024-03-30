83058177fdbc3e111d36a6b1a95b93da0a201203
Cesar Lopez <cesarlopezcorona.2000@gmail.com>
2024-03-30 17:40:01 -0600

    Error con el slideshow

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,73 @@
+import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
+import { ProductMobileSlideshow2 } from "@/components/product/slideshow/ProductMobileSlideshow2";
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from 'next/navigation';
 
-export default function () {
+interface Props {
+    params: {
+        slug: string;
+    }
+}
+
+export default function ({ params }: Props) {
+    const { slug } = params;
+    const product = initialData.products.find(product => product.slug === slug);
+
+    if (!product) {
+        notFound();
+    }
+
     return (
-        <div>
-            <h1>Product Page</h1>
+        <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+
+            <div className="col-span-1 md:col-span-2">
+
+                {/* Mobile Slideshow */}
+                <ProductMobileSlideshow2 className="block md:hidden" />
+                {/* <ProductMobileSlideshow
+                    className="block md:hidden"
+                    title={product.title}
+                    images={product.images}
+                /> */}
+                {/* Desktop Slideshow */}
+                <ProductSlideshow
+                    className="hidden md:block"
+                    title={product.title}
+                    images={product.images}
+                />
+
+            </div>
+
+
+            {/* Details */}
+            <div className="col-span-1 px-5|">
+                <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+                    {product.title}
+                </h1>
+                <p className="text-lg mb-5">${product.price.toFixed(2)}</p>
+
+
+                {/* Sizes Selector */}
+                <SizeSelector
+                    selectedSize={product.sizes[0]}
+                    availableSizes={product.sizes}
+                />
+
+                {/* Quantity Selector */}
+                <QuantitySelector quantity={2} />
+
+                {/* Button */}
+                <button className="btn-primary my-5">
+                    Add to cart
+                </button>
+
+                {/* Description */}
+                <h3 className="font-bold text-sm">Description</h3>
+                <p className="font-light">
+                    {product.description}
+                </p>
+            </div>
         </div>
     );
 }
