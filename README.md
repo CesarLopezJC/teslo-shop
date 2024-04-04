56edea98f8f487d822b230343d6d2afcc1889520
Cesar Lopez <cesarlopezcorona.2000@gmail.com>
2024-04-04 00:15:46 -0600

    Section 16: Preparation of database

--- README.md
+++ README.md
@@ -1,36 +1,12 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Description
+E-commers for publishing itemas such as clothes. shoes, hats, etc.
 
-## Getting Started
+## Diploy in development
+1. Clone repository
+2. Create a copy of ```.env.template```, rename to ```.env``` and change enviaronment variables
+3. Intall depends ```npm install```
+4. Deploy database  ```docker compose up -d```
+5. Execute seed ```npm run seed```
+5. Deploy proyect ```npm run dev```
 
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+## Diploy in production
---
+++ src/seed/seed-database.ts
@@ -0,0 +1,88 @@
+import { initialData } from "./seed";
+import prisma from '../lib/prisma';
+
+export { }
+
+async function main() {
+    await Promise.all([
+        await prisma.productImage.deleteMany(),
+        await prisma.product.deleteMany(),
+        await prisma.category.deleteMany(),
+    ])
+    // console.log(initialData);
+
+    const { categories, products } = initialData;
+    // Categories
+    // {
+    //     name: 'Shirt'
+    // }
+
+    const categoriesData = categories.map(category => ({
+        name: category
+    }))
+
+    await prisma.category.createMany({
+        data: categoriesData
+    });
+
+    const categoriesDB = await prisma.category.findMany();
+
+    const categoriesMap = categoriesDB.reduce((map, category) => {
+
+        map[category.name.toLowerCase()] = category.id;
+
+        return map;
+    }, {} as Record<string, string>); //<string=categoryString/label as shirt, string=categoryID
+
+    console.log(categoriesMap);
+    console.log('Seed executed successfully !');
+
+
+    //Products
+    // const { images, type, ...product1 } = products[0]; //when we use ...products we get the rest of variable
+
+    // This below function is for a row 
+    // await prisma.product.create({
+    //     data: {
+    //         ...product1,
+    //         categoryId: categoriesMap[type]
+    //     }
+    // })
+
+    // This function is for many rows 
+    products.forEach(async (product) => {
+        const { images, type, ...restOfProduct } = product;
+
+        const dbProduct = await prisma.product.create({
+            data: {
+                ...restOfProduct,
+                categoryId: categoriesMap[type]
+            }
+        })
+
+
+
+        // Images //Craet object array with all images
+        const imagesData = images.map(img => ({
+            url: img,
+            productId: dbProduct.id
+        }));
+
+        //insert all images in db
+        await prisma.productImage.createMany({
+            data: imagesData
+        })
+
+    });
+
+
+
+
+}
+
+
+(() => {
+    if (process.env.NODE_ENV === 'production') return;
+
+    main();
+})();
